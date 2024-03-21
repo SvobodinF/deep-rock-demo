c4207b7f6ab5160a72672c9718b895c65a55f620
_svobodin <artemkurdhy@gmail.com>
2024-03-22 01:10:44 +0300

    feat: level-1

--- Assets/_deep-rock/Source/Installers/Samples/PlayerInstaller.cs
+++ Assets/_deep-rock/Source/Installers/Samples/PlayerInstaller.cs
@@ -4,6 +4,7 @@ public class PlayerInstaller : Installer<PlayerConfiguration>
 {
     [Header("References")]
     [SerializeField] private MainCamera _mainCamera;
+    [SerializeField] private Transform _spawnPoint;
 
     [Header("Prefabs")]
     [SerializeField] private Player _player;
@@ -11,6 +12,7 @@ public class PlayerInstaller : Installer<PlayerConfiguration>
     public override void Install(PlayerConfiguration playerConfiguration)
     {
         Player player = Instantiate(_player);
+        player.transform.position = _spawnPoint.position;
         IController controller = Instantiate(playerConfiguration.InputController);
 
         PlayerData playerData = new PlayerData(controller, playerConfiguration.MovementConfiguration,
